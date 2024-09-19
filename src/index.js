f55f714072c1cc193a53a73db91085081b676163
banjy <1411074102@qq.com>
2024-09-19 17:22:28 +0800

    add help page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import HELP from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -33,6 +35,17 @@ function routeByHosts(host) {
 async function handleRequest(request) {
   const url = new URL(request.url);
   const upstream = routeByHosts(url.hostname);
+
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
+  }
+
   if (upstream === "") {
     return new Response(
       JSON.stringify({
