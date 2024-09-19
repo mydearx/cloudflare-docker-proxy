6f29b831d88a974fffc8e4d0251c3529904bf3ba
banjy <1411074102@qq.com>
2024-09-19 16:58:23 +0800

    change to own domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.banjy.icu": dockerHub,
+  "quay.banjy.icu": "https://quay.io",
+  "gcr.banjy.icu": "https://gcr.io",
+  "k8s-gcr.banjy.icu": "https://k8s.gcr.io",
+  "k8s.banjy.icu": "https://registry.k8s.io",
+  "ghcr.banjy.icu": "https://ghcr.io",
+  "cloudsmith.banjy.icu": "https://docker.cloudsmith.io",
+  "ecr.banjy.icu": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.banjy.icu": dockerHub,
 };
 
 function routeByHosts(host) {
